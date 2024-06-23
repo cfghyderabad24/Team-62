72e4fac7f612d4b79abd50f1b783b99ebb5d6ded
meghanapanyala <meghanapanyala05@gmail.com>
2024-06-23 05:51:10 +0530

    App.js changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import Filter from './components/filter/Filter';
 import Home from './components/Home';
 function App() {
   return (
